95b90fbdca0771ff28a080ba45ef06b1b25af38f
Ratakombat <janio.bzrr@gmail.com>
2023-10-04 16:39:46 -0300

    tentando corrigir erro do ProBuilder. Scripts para o inimigo foram adicionados

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    [SerializeField]
+    private Transform target;
+
+
+    private void Start() {
+        GetReferences();
+    }
+
+    private void Update() {
+        MoveToTarget();
+    }
+
+    private void MoveToTarget(){
+        agent.SetDestination(target.position);
+        RotateToTarget();
+    }  
+
+    private void RotateToTarget(){
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = rotation;
+    }
+
+
+    private void GetReferences(){
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+
+}
